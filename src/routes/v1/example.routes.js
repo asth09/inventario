10a6294d4aeaa4fee20e81956bec87e8a0ddfdab
asth09 <150283496+asth09@users.noreply.github.com>
2024-01-12 15:56:03 -0400

    login

--- src/routes/v1/example.routes.js
+++ src/routes/v1/example.routes.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const exampleController = require("../../controllers/example.controller")
+const login = require("../views/login")
 
 
 router.get("/", exampleController.getItem)
 router.get("/:id", exampleController.getDetail)
+router.get("/", login.getItem)
+/*router.get('/login'), (req, res) =>{
+    res.render('views/login');
+}*/
 
 
 module.exports = router
